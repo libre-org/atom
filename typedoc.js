c43672688fbbb1e056e49e1a8798c4fa0acf085c
Jelle Licht <jlicht@posteo.net>
2022-06-20 14:18:41 +0200

    [reword] Update deps.

--- typedoc.js
+++ typedoc.js
@@ -1,11 +1,8 @@
 module.exports = {
   excludeExternals: true,
-  excludeNotExported: true,
   excludePrivate: true,
   excludeProtected: true,
   hideGenerator: true,
-  includeDeclarations: true,
-  mode: "file",
   name: "@libre/atom",
   out: "./docs",
   theme: "default"
